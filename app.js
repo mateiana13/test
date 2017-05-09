48ea36726deba0491807cdf8a9642a695e54b7a1
Ana-Maria Matei <Ana-Maria.Matei@hub.vauban.ro>
2017-05-09 16:57:07 +0300

    make it better

--- app.js
+++ app.js
@@ -136,7 +136,7 @@ app.post('/webhook', function (req, res) {
       // Iterate over each messaging event
       pageEntry.messaging.forEach(function(messagingEvent) {
 
-        console.log(JSON.stringify(messagingEvent.message));
+        console.log("111111111111"+JSON.stringify(messagingEvent.message));
        //  var url;
        // if (messagingEvent.hasOwnProperty(url)){
 
