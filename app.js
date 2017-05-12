31ebe95d7d1db995f68d4b2bbf094afa8148c67d
ana matei <norway.vargsmal@gmail.com>
2017-05-12 17:02:20 +0300

    changes

--- app.js
+++ app.js
@@ -189,7 +189,7 @@ var getUrlContent = (url) => {
         console.log('body: '+body);
         var urlContent = JSON.stringify(body);
         console.log("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"+urlContent);
-        fs.writeFile("urlcont.txt", urlContent, function(err){
+        fs.writeFile("C:/PROJECTS/fbmesstut/urlcont.txt", urlContent, function(err){
           if(err){
             return console.log(err);
           }
