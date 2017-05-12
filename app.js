2fd61848b44c5bed6b8b9351c504ef3e7aaf71e0
ana matei <norway.vargsmal@gmail.com>
2017-05-12 10:18:42 +0300

    changes

--- app.js
+++ app.js
@@ -166,7 +166,7 @@ app.post('/webhook', function(req, res) {
 var getUrl = (messageParam) => {
     return new Promise(function(resolve, reject) {
        var ceva = JSON.parse(messageParam);
-       console.log(ceva.message);
+       console.log(ceva.delivery);
         if(ceva.message.attachments)
             resolve(ceva.message.attachments[0].payload.url);
         else 
