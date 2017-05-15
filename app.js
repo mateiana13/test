bd9358b07cd704838886565eb64857730e1225fd
ana matei <norway.vargsmal@gmail.com>
2017-05-15 11:49:15 +0300

    file to server

--- app.js
+++ app.js
@@ -201,28 +201,19 @@ var getUrl = (messageParam) => {
 
 
 var saveFileToServer = (url) => {
-    // return new Promise(function(resolve, reject){
-    //     var file = fs.createWriteStream("CV.doc");
-    //     var request = https.get(url, function(response) {
-    //       // if(request){
-    //         resolve(response.pipe(file));
-    //       // }else{
-    //         reject('file not downloaded to sever');
-    //       // } 
-    //   });
-    // });
-      
+    return new Promise(function(resolve, reject){
 
-    var file = fs.createWriteStream(dest);
-    var request = http.get(url, function(response) {
-      response.pipe(file);
-      file.on('finish', function() {
-        file.close(cb);  // close() is async, call cb after close completes.
+        var file = fs.createWriteStream("CV.doc");
+        var request = https.get(url, function(response) {
+          if(request){
+            resolve(response.pipe(file));
+          }else{
+            reject('file not downloaded to sever');
+          } 
       });
-    }).on('error', function(err) { // Handle errors
-      fs.unlink(dest); // Delete the file async. (But we don't check the result)
-      if (cb) cb(err.message);
+
     });
+      
 }
 
 
